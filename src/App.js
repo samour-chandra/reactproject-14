050d6fa6baca35f151f28bfb8250493482b91677
Samour chandra paul <scpsamour@gmail.com>
2026-02-14 23:58:54 +0600

    Add empty key to TabContent; minor whitespace
    Inserted an empty key prop on TabContent and adjusted spacing on Tab component props. The added key is currently empty (likely accidental) and will trigger React warnings or runtime issues; replace it with a stable unique value (for example activeTab or a content id) to fix. Whitespace changes to Tab props are cosmetic.

--- src/App.js
+++ src/App.js
@@ -32,14 +32,14 @@ function Tabbed({ content }) {
   return (
     <div>
       <div className="tabs">
-        <Tab num={0} activeTab={activeTab} onClick={setActiveTab} />
-        <Tab num={1} activeTab={activeTab} onClick={setActiveTab} />
-        <Tab num={2} activeTab={activeTab} onClick={setActiveTab} />
-        <Tab num={3} activeTab={activeTab} onClick={setActiveTab} />
+        <Tab num={0}  activeTab={activeTab} onClick={setActiveTab} />
+        <Tab num={1}  activeTab={activeTab} onClick={setActiveTab} />
+        <Tab num={2}  activeTab={activeTab} onClick={setActiveTab} />
+        <Tab num={3}  activeTab={activeTab} onClick={setActiveTab} />
       </div>
 
       {activeTab <= 2 ? (
-        <TabContent item={content.at(activeTab)} />
+        <TabContent item={content.at(activeTab)} key={}/>
       ) : (
         <DifferentContent />
       )}
